8c8beeef58ba951daa6e337502a18d6e17b80320
Josephat-Onkoba <josephatonkoba30@gmail.com>
2023-06-21 12:50:45 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace ClassObjectsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creating object 
+            Calculator calObject = new Calculator();
+
+            int result = calObject.CalculateSum(10, 20);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+
+    public class Calculator
+    {
+        public int CalculateSum(int no1, int no2)
+        {
+            return no1 + no2;
+        }
+    }
+}
